0190a00c0bae5a4aa844d75248f4b1d520eae7c3
Laszlo Agocs <laszlo.agocs@qt.io>
2022-05-12 16:37:17 +0200

    Improve scripts somewhat

--- prepare_deps.sh
+++ prepare_deps.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-cd tbb
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DTBB_TEST=Off -DBUILD_SHARED_LIBS=Off -DCMAKE_CXX_FLAGS=-D__TBB_DYNAMIC_LOAD_ENABLED=0 -DCMAKE_INSTALL_PREFIX=../deps .
-ninja install
-cd ..
-cd oidn
-cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DTBB_ROOT=../deps -DISPC_EXECUTABLE=$HOME/ispc/bin/ispc -DOIDN_STATIC_LIB=On -DOIDN_FILTER_RT=Off -DOIDN_APPS=Off -DCMAKE_INSTALL_PREFIX=../deps .
-ninja install
-cd ..
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DTBB_TEST=Off -DBUILD_SHARED_LIBS=Off -DCMAKE_CXX_FLAGS=-D__TBB_DYNAMIC_LOAD_ENABLED=0 -DCMAKE_INSTALL_PREFIX=build/install -S tbb -B build/tbb
+cmake --build build/tbb --target install
+cmake -GNinja -DCMAKE_BUILD_TYPE=Release -DTBB_ROOT=build/install -DISPC_EXECUTABLE=$HOME/ispc/bin/ispc -DOIDN_STATIC_LIB=On -DOIDN_FILTER_RT=Off -DOIDN_APPS=Off -DCMAKE_INSTALL_PREFIX=build/install -S oidn -B build/oidn
+cmake --build build/oidn --target install
